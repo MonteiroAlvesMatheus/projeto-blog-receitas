1d570ab5995620d868deacc7f521f54f7771fbd6
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2024-11-27 15:52:21 -0300

    Remove contato and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # HTTP Request
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
