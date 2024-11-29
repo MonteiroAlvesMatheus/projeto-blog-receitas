b499f6683565dc5872d673ea22155d72542f5168
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2024-11-29 15:12:40 -0300

    add blocks

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from __localcode.main import make_recipe
 
 
 # HTTP Request
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
