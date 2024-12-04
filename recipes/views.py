eee74514a7c01709873599b04071db536f3a394a
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2024-12-04 14:49:59 -0300

    add object views, 404 and titles

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from __localcode.main import make_recipe
 from .models import Recipe
 
@@ -14,17 +14,21 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |'
     })
 
 
 def recipe(request, id):
 
-    recipes = Recipe.objects.filter(id=id).order_by('-id')
+    recipe = get_object_or_404(Recipe,
+                               id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipes,
+        'recipe': recipe,
+        'is_detail_page': True,
+        'title': f'{recipe.title} |'
     })
