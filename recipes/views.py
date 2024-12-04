1c6645ce9a3be24131ea48418684f7f9ef011c14
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2024-12-04 08:37:52 -0300

    creating category view and creating recipes on admin mode

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from __localcode.main import make_recipe
-
+from .models import Recipe
 
 # HTTP Request
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
     })
 
 
 def recipe(request, id):
+
+    recipes = Recipe.objects.filter(id=id).order_by('-id')
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
-        'is_detail_page': True,
+        'recipe': recipes,
     })
