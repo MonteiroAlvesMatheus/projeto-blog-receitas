c75b58752bc85e2e4714c53566946668e510aa2a
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2025-01-17 13:56:55 -0300

    Creating tests views, base and url

---
+++ recipes/tests/test_recipe_base.py
@@ -0,0 +1,57 @@
+from django.test import TestCase  # type: ignore
+from recipes.models import Category, Recipe, User
+
+
+class RecipeTestBase(TestCase):
+    def setUp(self):
+        return super().setUp()
+
+    def make_category(self, name='Category'):
+        return Category.objects.create(name=name)
+
+    def make_author(self,
+                    username='teste_user',
+                    email='teste@teste.com',
+                    password='123',
+                    first_name='testeuser',
+                    last_name='teste'
+                    ):
+        return User.objects.create_user(
+            username=username, email=email, password=password,
+            first_name=first_name, last_name=last_name)
+
+    def make_recipe(
+            self,
+            title='teste title',
+            description='descrição teste',
+            slug='recoe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation steps',
+            preparations_steps_is_html=False,
+            is_published=True,
+            category_data=None,
+            author_data=None):
+
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(
+            title=title,
+            description=description,
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit=preparation_time_unit,
+            servings=servings,
+            servings_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparations_steps_is_html=preparations_steps_is_html,
+            is_published=is_published,
+            category=self.make_category(**category_data),
+            author=self.make_author(**author_data)
+        )
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,22 +1,62 @@
 from django.test import TestCase  # type: ignore
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase
 # Create your tests here.
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+
     def test_recipe_home_view_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_templates(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_templates_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes published',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipe = response.context['recipes']
+        self.assertIn('teste title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipe), 1)
+
     def test_recipe_category_view_is_correct(self):
         view = resolve(
-            reverse('recipes:category', kwargs={'category_id': 1})
+            reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertIs(view.func, views.category)
+
+    def test_recipe_category_view_returns_status_code_404_if_not_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_view_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_status_code_404_if_not_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
