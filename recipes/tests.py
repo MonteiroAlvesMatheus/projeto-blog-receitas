569602705b8aac854965286ff1a59e815e12148d
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2025-01-17 08:54:18 -0300

    update tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -5,4 +5,6 @@ from django.test import TestCase  # type: ignore
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        variavel = '123456'
+        print('Olá Mundo')
+        assert 1 == 1, 'Um é igual a um'
