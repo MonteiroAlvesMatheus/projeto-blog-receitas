7d5b611700eab0b7e51ca9e36a7fd78d6c77d721
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2025-01-22 09:48:30 -0300

    Update tests views

---
+++ recipes/tests/test_recipe_detail_view.py
@@ -0,0 +1,39 @@
+from django.urls import reverse, resolve
+from recipes import views
+from .test_recipe_base import RecipeTestBase
+# Create your tests here.
+
+
+class RecipeDetailViewsTest(RecipeTestBase):
+    def test_recipe_detail_view_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_status_code_404_if_not_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_recipes(self):
+        needed_title = 'This is a datail page - It Load one recipe'
+
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_doesnt_load_recipes_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.pk})
+        )
+
+        self.assertEqual(response.status_code, 404)
---
+++ recipes/tests/test_recipe_home_view.py
@@ -0,0 +1,46 @@
+from django.urls import reverse, resolve
+from recipes import views
+from .test_recipe_base import RecipeTestBase
+# Create your tests here.
+
+
+class RecipeHomeViewsTest(RecipeTestBase):
+
+    def test_recipe_home_view_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_templates(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_templates_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes published',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipe = response.context['recipes']
+
+        self.assertIn('teste title', content)
+        self.assertEqual(len(response_context_recipe), 1)
+
+    def test_recipe_home_template_doesnt_load_recipes_not_published(self):
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            'No recipes published',
+            response.content.decode('utf-8')
+        )
