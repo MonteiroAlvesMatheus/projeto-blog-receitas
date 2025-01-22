82c486e5869cfb0a83751a1d6b9511d2f3915f7a
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2025-01-22 15:34:26 -0300

    creating paginator

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,9 @@
 from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import Recipe
+
 
 # HTTP Request
 
@@ -9,8 +11,12 @@ from .models import Recipe
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
