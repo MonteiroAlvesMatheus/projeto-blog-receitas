f525e47d0cf1ed89b77525b8094510cfca455eff
Matheus Alves Monteiro <mateus.a.mont@gmail.com>
2025-02-18 09:10:44 -0300

    adjusting errors

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from recipes.models import Recipe
 from utils.django_forms import add_attr
 from collections import defaultdict
@@ -7,6 +8,9 @@ from collections import defaultdict
 class RecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
+
         add_attr(self.fields['preparation_steps'], 'class', 'span-2')
 
     class Meta:
@@ -40,3 +44,23 @@ class RecipeForm(forms.ModelForm):
                 )
             ),
         }
+
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+
+        cleaned_data = self.cleaned_data
+
+        title = cleaned_data.get('title')
+        description = cleaned_data.get('description')
+
+        if len(title) < 5:
+            self._my_errors['title'].append('Must have at least 5 chars')
+
+        if title == description:
+            self._my_errors['title'].append('Cannot be equal to description')
+            self._my_errors['description'].append('Cannot be equal to title')
+
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
+
+        return super_clean
